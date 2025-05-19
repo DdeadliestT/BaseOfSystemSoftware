b3ca17da00f98e982b3c1757c000b67cfd05b0f1
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2025-05-19 14:12:25 +0300

    Added scripts for general part

---
+++ LabWork2/createDirWithFiles.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# go to into firstcat
+cd firstcat 
+
+# create firstfile
+echo > firstfile.txt
+
+cd secondcat; echo > secondfile.txt
+
+cd thirdcat; echo > thirdfile.txt
---
+++ LabWork2/first_script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+stty -echo
+
+# создание new catalog
+mkdir firstcat
+
+# go to into new catalog
+cd firstcat
+
+mkdir secondcat; cd secondcat; mkdir thirdcat
