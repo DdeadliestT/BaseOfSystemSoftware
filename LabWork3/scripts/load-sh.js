a564c9b904ee4f18758cc8ed063eadce588a00ec
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2025-05-26 22:32:34 +0300

    All code have been formated for readebly

--- LabWork3/scripts/load-sh.js
+++ LabWork3/scripts/load-sh.js
@@ -1,13 +1,13 @@
 function fetchScript(url, elementId) {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) throw new Error("Ошибка при загрузке файла");
-        return response.text();
-      })
-      .then(data => {
-        document.getElementById(elementId).textContent = data;
-      })
-      .catch(error => {
-        document.getElementById(elementId).textContent = "Не удалось загрузить файл:\n" + error.message;
-      });
-  }
+  fetch(url)
+    .then(response => {
+      if (!response.ok) throw new Error("Ошибка при загрузке файла");
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById(elementId).textContent = data;
+    })
+    .catch(error => {
+      document.getElementById(elementId).textContent = "Не удалось загрузить файл:\n" + error.message;
+    });
+}
--- LabWork3/scripts/task1.js
+++ LabWork3/scripts/task1.js
@@ -2,13 +2,13 @@ function findMin() {
 	const a = parseFloat(document.getElementById("numA").value);
 	const b = parseFloat(document.getElementById("numB").value);
 	const resultElement = document.getElementById("result");
-  
+
 	if (isNaN(a) || isNaN(b)) {
-	  resultElement.textContent = "Введите оба числа.";
-	  resultElement.style.color = "#ff6666";
-	  return;
+		resultElement.textContent = "Введите оба числа.";
+		resultElement.style.color = "#ff6666";
+		return;
 	}
-  
+
 	const min = Math.min(a, b);
 	resultElement.textContent = `Минимум: ${min}`;
 	resultElement.style.color = "#00ffff";
